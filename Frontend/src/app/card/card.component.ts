70cd8f257ee567c2be7c1636c7a39cf769144c9a
Manu-Andaluz <manupeandaluz@gmail.com>
2024-03-05 18:32:53 -0300

    adding props to the cards

--- Frontend/src/app/card/card.component.ts
+++ Frontend/src/app/card/card.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   standalone: true,
   imports: [],
   templateUrl: './card.component.html',
-  styleUrl: './card.component.scss'
+  styleUrl: './card.component.scss',
 })
 export class CardComponent {
-
+  @Input() image_link: string = '';
+  @Input() website_name: string = '';
+  @Input() website_url: string = '';
+  @Input() password_quantity: number = 1;
 }
