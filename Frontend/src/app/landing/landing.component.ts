d424ddf500dcf4e89ae70b8d6726e398325ef406
Manu-Andaluz <manupeandaluz@gmail.com>
2024-03-05 18:00:29 -0300

    adding cards

--- Frontend/src/app/landing/landing.component.ts
+++ Frontend/src/app/landing/landing.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../card/card.component';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss',
 })
