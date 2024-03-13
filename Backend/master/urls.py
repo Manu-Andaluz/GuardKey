a5257fa167fb49e08a55ab96c85b39989a3a00fd
Manu-Andaluz <manupeandaluz@gmail.com>
2024-03-13 12:30:00 -0300

    create file and loads

--- Backend/master/urls.py
+++ Backend/master/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("example/", views.example, name="example")
+    path("generate-key/", views.generate_key, name="example")
 ]
--- Backend/master/views.py
+++ Backend/master/views.py
@@ -3,8 +3,7 @@ from django.shortcuts import render
 from .models import PasswordManager
 from .serializers import PasswordManagerSerializer
 
-# Create your views here.
-def example(request):
+def generate_key(request):
     password_manager = PasswordManager() 
     key = password_manager.create_key("mykey.key")
     serializer = PasswordManagerSerializer({'key': key})
