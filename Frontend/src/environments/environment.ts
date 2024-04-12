7e442bf2e6de485e61a176a5c2d6c9b6ecd4adb5
Manu-Andaluz <manupeandaluz@gmail.com>
2024-04-12 12:00:29 -0300

    adding new packages

--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  //apiPath: 'https://guardkey-production.up.railway.app',
-  apiPath: 'http://3.142.130.166:8000',
+  apiPath: 'https://guardkey-production.up.railway.app', // railway ( testers use )
+  // apiPath: 'http://3.142.130.166:8000', // aws ec2 ( personal use )
 };
