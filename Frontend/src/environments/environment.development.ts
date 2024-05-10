e6380464c1f3313796ee2d25cc581e624e95f94e
Manu-Andaluz <manupeandaluz@gmail.com>
2024-05-10 17:06:03 -0300

    changing path

--- Frontend/src/environments/environment.development.ts
+++ Frontend/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   //apiPath: 'http://localhost:8000',
   //apiPath: 'https://manuandaluz.pythonanywhere.com',
-  apiPath: 'http://3.142.130.166:8000', // aws ec2 ( personal use )
-  //apiPath: 'https://guardkey-production.up.railway.app', // railway ( testers use )
+  //apiPath: 'http://3.142.130.166:8000', // aws ec2 ( personal use )
+  apiPath: 'https://guardkey-production.up.railway.app', // railway ( testers use )
 };
